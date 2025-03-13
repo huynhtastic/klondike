f918e3f9f5cc42ebfeed66ccdefb59970b4b6bfd
Richard Huynh <huynhtastic@gmail.com>
2025-03-13 16:02:18 -0500

    feat: add interactions with stock pile

--- lib/components/stock.dart
+++ lib/components/stock.dart
@@ -1,6 +1,56 @@
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/extensions.dart';
+import 'package:flutter/material.dart' show Paint, PaintingStyle;
 
-class Stock extends PositionComponent {
+import '../klondike_game.dart';
+import 'card.dart';
+import 'waste.dart';
+
+final _borderPaint =
+    Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 10
+      ..color = const Color(0xFF3F5B5D);
+final _circlePaint =
+    Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 100
+      ..color = const Color(0x883F5B5D);
+
+class StockPile extends PositionComponent with TapCallbacks {
+  StockPile({super.position}) : super(size: KlondikeGame.cardSize);
+
+  /// Which cards are currently placed onto this pile. The first card in the
+  /// list is at the bottom, the last card is on top.
+  final List<Card> _cards = [];
+
+  void acquireCard(Card card) {
+    assert(!card.isFaceUp);
+    card.position = position;
+    card.priority = _cards.length;
+    _cards.add(card);
+  }
+
   @override
-  bool get debugMode => true;
+  void onTapUp(TapUpEvent event) {
+    final wastePile = parent!.firstChild<WastePile>()!;
+    for (var i = 0; i < 3; i++) {
+      if (_cards.isNotEmpty) {
+        final card = _cards.removeLast();
+        card.flip();
+        wastePile.acquireCard(card);
+      }
+    }
+  }
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRRect(KlondikeGame.cardRRect, _borderPaint);
+    canvas.drawCircle(
+      Offset(width / 2, height / 2),
+      KlondikeGame.cardWidth * 0.3,
+      _circlePaint,
+    );
+  }
 }
--- lib/klondike_game.dart
+++ lib/klondike_game.dart
@@ -1,11 +1,10 @@
-import 'dart:math';
-
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/flame.dart';
-import 'package:klondike/components/foundation.dart';
+import 'package:flutter/material.dart' show RRect, Rect, Radius;
 
 import 'components/card.dart';
+import 'components/foundation.dart';
 import 'components/pile.dart';
 import 'components/stock.dart';
 import 'components/waste.dart';
@@ -18,24 +17,28 @@ class KlondikeGame extends FlameGame {
   static const double cardGap = 175.0;
   static const double cardRadius = 100.0;
   static final Vector2 cardSize = Vector2(cardWidth, cardHeight);
+  static final cardRRect = RRect.fromRectAndRadius(
+    const Rect.fromLTWH(0, 0, cardWidth, cardHeight),
+    const Radius.circular(cardRadius),
+  );
 
   @override
   Future<void> onLoad() async {
     await Flame.images.load(spriteFile);
     final stock =
-        Stock()
+        StockPile()
           ..size = cardSize
           ..position = Vector2(cardGap, cardGap);
 
     final waste =
-        Waste()
+        WastePile()
           ..size = cardSize
           ..position = Vector2(cardWidth + 2 * cardGap, cardGap);
 
     final foundations = List.generate(
       4,
       (i) =>
-          Foundation()
+          FoundationPile()
             ..size = cardSize
             ..position = Vector2(
               (i + 3) * (cardWidth + cardGap) + cardGap,
@@ -46,7 +49,7 @@ class KlondikeGame extends FlameGame {
     final piles = List.generate(
       7,
       (i) =>
-          Pile()
+          TableauPile()
             ..size = cardSize
             ..position = Vector2(
               cardGap + i * (cardWidth + cardGap),
@@ -66,19 +69,27 @@ class KlondikeGame extends FlameGame {
     camera.viewfinder.position = Vector2(worldWidth / 2, 0);
     camera.viewfinder.anchor = Anchor.topCenter;
 
-    final random = Random();
-    for (var i = 0; i < 7; i++) {
-      for (var j = 0; j < 4; j++) {
-        final card =
-            Card(random.nextInt(13) + 1, random.nextInt(4))
-              ..position = Vector2(100 + i * 1150, 100 + j * 1500)
-              ..addToParent(world);
-        if (random.nextDouble() < 0.9) {
-          // flip face up with 90% probability
-          card.flip();
-        }
-      }
-    }
+    // this was only used to generate the initial layout for debugging
+    // final random = Random();
+    // for (var i = 0; i < 7; i++) {
+    //   for (var j = 0; j < 4; j++) {
+    //     final card =
+    //         Card(random.nextInt(13) + 1, random.nextInt(4))
+    //           ..position = Vector2(100 + i * 1150, 100 + j * 1500)
+    //           ..addToParent(world);
+    //     if (random.nextDouble() < 0.9) {
+    //       // flip face up with 90% probability
+    //       card.flip();
+    //     }
+    //   }
+    // }
+    final cards = [
+      for (var rank = 1; rank <= 13; rank++)
+        for (var suit = 0; suit < 4; suit++) Card(rank, suit),
+    ];
+    world.addAll(cards);
+    cards.shuffle();
+    cards.forEach(stock.acquireCard);
   }
 }
 
